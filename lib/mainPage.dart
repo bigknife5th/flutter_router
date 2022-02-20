1ab1b572b9b557abc88e4990cde09a8d036d625e
shawn <you@example.com>
2022-02-20 13:52:51 +0800

    del

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'localizations.dart';
-
 class PageMain extends StatelessWidget {
   const PageMain({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: AppLocalizations.of(context).appTitle,
+        //title: AppLocalizations.of(context).appTitle,
         home: Scaffold(
             appBar: AppBar(
-          //title: Text(AppLocalizations.of(context).appTitle),
-          title: const Text("Smart"),
-        )));
+      //title: Text(AppLocalizations.of(context).appTitle),
+      title: const Text("Smart"),
+    )));
   }
 }
